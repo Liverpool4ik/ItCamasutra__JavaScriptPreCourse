0c228ab5beea7857888876e7bb09944a2061927a
Liverpool4ik <liverpool4ik@proton.me>
2024-04-21 23:34:59 +0300

    first commit

---
+++ 01. Sprint-01/Lesson_2/1.__Kuzyberdin_version/main.js
@@ -0,0 +1,39 @@
+// data
+const playlist = {
+	title: "Hip-Hop Hits",
+	coverImageUrl: "./images/mainCoverImage.jpg",
+	info: {
+		totalTracks: 4,
+		totalTracksDurationInSeconds: 733,
+	},
+	tracks: [
+		{
+			trackCoverImageUrl: "./images/image-1.jpg",
+			ArtistName: "Eminem",
+			trackTitle: "Rap God",
+			trackUrl: "./musicTracks/playListHipHop/track-1.mp3",
+			isPopular: true,
+		},
+		{
+			trackCoverImageUrl: "./images/image-2.jpg",
+			ArtistName: "50 cent",
+			trackTitle: "In da Club",
+			trackUrl: "./musicTracks/playListHipHop/track-2.mp3",
+			isPopular: true,
+		},
+		{
+			trackCoverImageUrl: "./images/image-3.jpg",
+			ArtistName: "DMX",
+			trackTitle: "X Gon'Give It To YA",
+			trackUrl: "./musicTracks/playListHipHop/track-3.mp3",
+			isPopular: false,
+		},
+		{
+			trackCoverImageUrl: "./images/image-4.jpg",
+			ArtistName: "Eminem feat 50 cent, Cashia, Lloyd Banks",
+			trackTitle: "You Don't Know",
+			trackUrl: "./musicTracks/playListHipHop/track-4.mp3",
+			isPopular: false,
+		},
+	],
+};
